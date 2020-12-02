1e5c74475750644958b78bc6a5fedc2dcb5559fe
rasulse <rasulse@mail.ru>
2020-12-02 15:06:12 +0300

    Less0ns_1

---
+++ Индекс Массы тела/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Индекс_Массы_тела
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ввести вес и рост человека.Рассчитать и вывести индекс массы тела(ИМТ) по формуле
+            //I = m / (h * h); где m — масса тела в килограммах, h — рост в метрах.
+
+            double m;
+            double h;
+            double i;
+            Console.Write("Введите свой вес: ");
+            string ves = Console.ReadLine();
+            m = Convert.ToDouble(ves);
+
+            Console.Write("Введите свой рост в метрах: ");
+            string rost = Console.ReadLine();
+            h = Convert.ToDouble(rost);
+
+            i = m / (h * h);
+            
+            Console.WriteLine("Индекс массы тела равен: {0:F}", i);
+            Console.ReadLine();
+        }
+    }
+}
